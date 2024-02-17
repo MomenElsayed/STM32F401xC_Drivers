3f57ef20b54db729f51bab5bab56a294c8adc361
Momen Elsayed <momen98s@gmail.com>
2024-02-17 20:30:46 +0200

    GPIO V01

---
+++ 01_MCAL/01_GPIO/GPIO.c
@@ -0,0 +1,156 @@
+/******************************************************************************
+ *
+ * Module: GPIO
+ *
+ * File Name: GPIO.c
+ *
+ * Description: Source file for the GPIO driver Configurations for STM32F401xC
+ *
+ * Author: Momen Elsayed Shaban
+ *
+ *******************************************************************************/
+
+#include "GPIO.h"
+
+#define GPIO_CLR_MASK				0x00000003U
+#define GPIO_MODE_MASK				0x00000003U		/*Input,output,AF or AN*/
+#define GPIO_TYPE_MASK				0x00000001U		/*PushPull or OpenDrain*/
+#define GPIO_PUPD_MASK				0x00000018U
+#define GPIO_BSRR_RESET_OFFSET		16U
+
+#define IS_GPIO_MODE(MODE) (((MODE) == GPIO_MODE_IN_FL)               ||\
+                            ((MODE) == GPIO_MODE_IN_PU)               ||\
+                            ((MODE) == GPIO_MODE_IN_PD)               ||\
+                            ((MODE) == GPIO_MODE_OUT_PP)              ||\
+                            ((MODE) == GPIO_MODE_OUT_PP_PU)           ||\
+                            ((MODE) == GPIO_MODE_OUT_PP_PD)           ||\
+                            ((MODE) == GPIO_MODE_OUT_OD)              ||\
+                            ((MODE) == GPIO_MODE_OUT_OD_PU)           ||\
+                            ((MODE) == GPIO_MODE_OUT_OD_PD)           ||\
+                            ((MODE) == GPIO_MODE_AF_PP)               ||\
+                            ((MODE) == GPIO_MODE_AF_PP_PU)            ||\
+                            ((MODE) == GPIO_MODE_AF_PP_PD)            ||\
+                            ((MODE) == GPIO_MODE_AF_OD)               ||\
+                            ((MODE) == GPIO_MODE_AF_OD_PU)            ||\
+                            ((MODE) == GPIO_MODE_AF_OD_PD)            ||\
+                            ((MODE) == GPIO_MODE_AN))
+
+typedef struct{
+	volatile u32 MODER;
+	volatile u32 OTYPER;
+	volatile u32 OSPEEDR;
+	volatile u32 PUPDR;
+	volatile u32 IDR;
+	volatile u32 ODR;
+	volatile u32 BSRR;
+	volatile u32 LCKR;
+	volatile u32 AFRL;
+	volatile u32 AFRH;
+}GPIO_Registers_t;
+
+GPIO_ErrorStatus_t GPIO_Init(GPIO_Pin_t* GPIOx)
+{
+	GPIO_ErrorStatus_t Error_status = GPIO_OK;
+	if(GPIOx->GPIO_Pin > GPIO_PIN_15)
+	{
+		Error_status = GPIO_InvalidPin;
+	}
+	else if(GPIOx->GPIO_Port > GPIO_PORT_C)
+	{
+		Error_status = GPIO_InvalidPort;
+	}
+	else if(!(IS_GPIO_MODE(GPIOx->GPIO_Mode)))
+	{
+		Error_status = GPIO_InvalidMode;
+	}
+	else if(GPIOx->GPIO_Speed > GPIO_SPEED_VHIGH)
+	{
+		Error_status = GPIO_InvalidSpeed;
+	}
+	else{
+		/*Do Nothing*/
+	}
+
+	if(Error_status == GPIO_OK)
+	{
+		u32 loc_temp_MODER = ((GPIO_Registers_t*)GPIOx->GPIO_Port)->MODER;
+		u32 loc_temp_PUPDR = ((GPIO_Registers_t*)GPIOx->GPIO_Port)->PUPDR;
+		u32 loc_temp_OSPEEDR = ((GPIO_Registers_t*)GPIOx->GPIO_Port)->OSPEEDR;
+		/**********************************Configure MODER**************************************/
+		loc_temp_MODER &= ~(GPIO_CLR_MASK << (2U * GPIOx->GPIO_Pin));		/*Each Pin has 2 bits in the MODER*/
+		loc_temp_MODER |= (GPIOx->GPIO_Mode & GPIO_MODE_MASK) << (2U * GPIOx->GPIO_Pin);
+		((GPIO_Registers_t*)GPIOx->GPIO_Port)->MODER = loc_temp_MODER;
+
+		/**********************************Configure OTYPER**************************************/
+		((GPIO_Registers_t*)GPIOx->GPIO_Port)->OTYPER |= ((GPIOx->GPIO_Mode & GPIO_TYPE_MASK) >> 2U) << (GPIOx->GPIO_Pin);
+
+		/**********************************Configure PU/PD**************************************/
+		loc_temp_PUPDR &= ~(GPIO_CLR_MASK << (2U * GPIOx->GPIO_Pin));
+		loc_temp_PUPDR |= ((GPIOx->GPIO_Mode & GPIO_PUPD_MASK) >> 3U) << (2U * GPIOx->GPIO_Pin);	/*Fourth bit contains the info of PU and PD in the MODE Value so shifting by 3*/
+		((GPIO_Registers_t*)GPIOx->GPIO_Port)->PUPDR = loc_temp_PUPDR;
+
+		/**********************************Configure Speed**************************************/
+		loc_temp_OSPEEDR &= ~(GPIO_CLR_MASK << (2U * GPIOx->GPIO_Pin));
+		loc_temp_OSPEEDR |= (GPIOx->GPIO_Speed) << (2U * GPIOx->GPIO_Pin);
+		((GPIO_Registers_t*)GPIOx->GPIO_Port)->OSPEEDR = loc_temp_OSPEEDR;
+	}
+	return Error_status;
+}
+
+
+
+GPIO_ErrorStatus_t GPIO_setPinValue(void* GPIO_Port, u8 GPIO_pin, u8 GPIO_State)
+{
+	GPIO_ErrorStatus_t Error_status = GPIO_OK;
+	if(GPIO_Port == NULL_PTR)
+	{
+		Error_status = GPIO_NULLPTR;
+	}
+	else if (GPIO_pin > GPIO_PIN_15)
+	{
+		Error_status = GPIO_InvalidPin;
+	}
+	else{
+		/*Do Nothing*/
+	}
+	if (Error_status == GPIO_OK)
+	{
+		switch (GPIO_State)
+		{
+		case GPIO_STATE_SET:
+			((GPIO_Registers_t *)GPIO_Port)->BSRR = GPIO_STATE_SET << GPIO_pin;
+			break;
+		
+		case GPIO_STATE_RESET:
+			((GPIO_Registers_t *)GPIO_Port)->BSRR = 1 << (GPIO_pin + GPIO_BSRR_RESET_OFFSET);
+			break;
+
+		default:
+			Error_status = GPIO_InvalidState;
+			break;
+		}
+	}
+	return Error_status;
+}
+
+
+GPIO_ErrorStatus_t GPIO_getPinValue(void* GPIO_Port, u8 GPIO_pin, u8* GPIO_State)
+{
+	GPIO_ErrorStatus_t Error_status = GPIO_OK;
+	if(GPIO_Port == NULL_PTR || GPIO_State == NULL_PTR)
+	{
+		Error_status = GPIO_NULLPTR;
+	}
+	else if (GPIO_pin > GPIO_PIN_15)
+	{
+		Error_status = GPIO_InvalidPin;
+	}
+	else{
+		/*Do Nothing*/
+	}
+	if(Error_status == GPIO_OK)
+	{
+		*GPIO_State = (((GPIO_Registers_t*)GPIO_Port)->IDR & (1 << GPIO_pin)) ? GPIO_STATE_SET : GPIO_STATE_RESET;
+	}
+	return Error_status;
+}
---
+++ 01_MCAL/01_GPIO/GPIO.h
@@ -0,0 +1,175 @@
+/******************************************************************************
+ *
+ * Module: GPIO
+ *
+ * File Name: GPIO.h
+ *
+ * Description: Header file for the GPIO driver Configurations for STM32F401xC
+ *
+ * Author: Momen Elsayed Shaban
+ *
+ *******************************************************************************/
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "std_types.h"
+
+/*******************************************************************************
+ *                                Type Decelerations                           *
+ *******************************************************************************/
+/*******Macros for the GPIO pins*******/
+#define GPIO_PIN_0  		0x00000000
+#define GPIO_PIN_1  		0x00000001
+#define GPIO_PIN_2  		0x00000002
+#define GPIO_PIN_3  		0x00000003
+#define GPIO_PIN_4  		0x00000004
+#define GPIO_PIN_5  		0x00000005
+#define GPIO_PIN_6  		0x00000006
+#define GPIO_PIN_7  		0x00000007
+#define GPIO_PIN_8  		0x00000008
+#define GPIO_PIN_9  		0x00000009
+#define GPIO_PIN_10 		0x0000000A
+#define GPIO_PIN_11 		0x0000000B
+#define GPIO_PIN_12 		0x0000000C
+#define GPIO_PIN_13 		0x0000000D
+#define GPIO_PIN_14 		0x0000000E
+#define GPIO_PIN_15 		0x0000000F
+
+/*******Macros for the GPIO ports*******/
+#define GPIO_PORT_A  		(void*)0x40020000 /*casted to void to be used for any hardware with different rgister size*/
+#define GPIO_PORT_B  		(void*)0x40020400
+#define GPIO_PORT_C  		(void*)0x40020800
+
+
+/*******Macros for the GPIO pin modes*******/
+#define GPIO_MODE_IN_FL 	  0x00000000
+#define GPIO_MODE_IN_PU 	  0x00000008
+#define GPIO_MODE_IN_PD 	  0x00000010
+
+#define GPIO_MODE_OUT_PP   	0x00000001
+#define GPIO_MODE_OUT_PP_PU	0x00000009
+#define GPIO_MODE_OUT_PP_PD	0x00000011
+#define GPIO_MODE_OUT_OD   	0x00000005
+#define GPIO_MODE_OUT_OD_PU	0x0000000D
+#define GPIO_MODE_OUT_OD_PD	0x00000015
+
+#define GPIO_MODE_AF_PP    	0x00000002
+#define GPIO_MODE_AF_PP_PU 	0x0000000A
+#define GPIO_MODE_AF_PP_PD 	0x00000012
+#define GPIO_MODE_AF_OD    	0x00000006
+#define GPIO_MODE_AF_OD_PU 	0x0000000E
+#define GPIO_MODE_AF_OD_PD 	0x00000016
+
+#define GPIO_MODE_AN       	0x00000003
+
+/*******Macros for the GPIO pin speeds*******/
+#define GPIO_SPEED_LOW     	0x00000000
+#define GPIO_SPEED_MED     	0x00000001
+#define GPIO_SPEED_HIGH    	0x00000002
+#define GPIO_SPEED_VHIGH   	0x00000003
+
+#define GPIO_STATE_RESET    0
+#define GPIO_STATE_SET      1
+
+typedef struct
+{
+  u32 GPIO_Pin;
+  void* GPIO_Port;
+  u32 GPIO_Speed;
+  u32 GPIO_Mode;
+}GPIO_Pin_t;
+
+typedef enum{
+    GPIO_OK,
+    GPIO_InvalidPort,
+    GPIO_InvalidPin,
+    GPIO_InvalidMode,
+    GPIO_InvalidSpeed,
+    GPIO_NULLPTR,
+    GPIO_InvalidState,
+    GPIO_NOK
+}GPIO_ErrorStatus_t;
+
+/*******************************************************************************
+ *                              Functions Prototypes                           *
+ *******************************************************************************/
+/*****************************************************
+ * Function: GPIO_Init
+ * Description: Initializes the specified GPIO pin based on the provided configuration.
+ *
+ * Parameters:
+ *   - GPIOx: Pointer to a GPIO_Pin_t structure containing pin configuration.
+ *
+ * Return:
+ *   - GPIO_ErrorStatus_t: An error status indicating the success or failure of the GPIO initialization.
+ *
+ * Errors:
+ *   - GPIO_InvalidPin: Returned if an invalid GPIO pin is provided.
+ *   - GPIO_InvalidPort: Returned if an invalid GPIO port is provided.
+ *   - GPIO_InvalidMode: Returned if an invalid GPIO mode is provided.
+ *   - GPIO_InvalidSpeed: Returned if an invalid GPIO speed is provided.
+ *   - GPIO_OK: Returned if the GPIO initialization is successful.
+ *
+ * Usage: GPIO_ErrorStatus_t error = GPIO_Init(&gpioPinConfig); 
+ * Example: Configure a GPIO pin with the specified settings.
+ *
+ * Notes:
+ *   - Ensure that no errors are present before using the GPIO pin.
+ *   - Before Using GPIO Ensure that you are enabling the clock for the periphral by using RCC Driver
+ *   - The GPIO_Pin_t structure holds the configuration for GPIO pins, including pin number, port, speed, and mode.
+ *****************************************************/
+GPIO_ErrorStatus_t GPIO_Init(GPIO_Pin_t* GPIOx);
+
+/*****************************************************
+ * Function: GPIO_setPinValue
+ * Description: Sets or resets the state of a specified GPIO pin.
+ *
+ * Parameters:
+ *   - GPIO_Port: Pointer to the GPIO port to which the pin belongs.
+ *   - GPIO_pin: The GPIO pin number to be set or reset.
+ *   - GPIO_State: The desired state to set the GPIO pin (GPIO_STATE_SET or GPIO_STATE_RESET).
+ *
+ * Return:
+ *   - GPIO_ErrorStatus_t: An error status indicating the success or failure of the operation.
+ *
+ * Errors:
+ *   - GPIO_NULLPTR: Returned if the GPIO_Port pointer is NULL.
+ *   - GPIO_InvalidPin: Returned if an invalid GPIO pin is provided.
+ *   - GPIO_InvalidState: Returned if an invalid GPIO state is provided.
+ *   - GPIO_OK: Returned if the operation is successful.
+ *
+ * Usage: GPIO_ErrorStatus_t error = GPIO_setPinValue(GPIO_PORT_A, GPIO_PIN_5, GPIO_STATE_SET);
+ * Example: Set GPIO pin 5 on port A to a high state.
+ *
+ * Notes:
+ *   - Ensure that no errors are present before using the GPIO pin.
+ *****************************************************/
+GPIO_ErrorStatus_t GPIO_setPinValue(void* GPIO_Port, u8 GPIO_pin, u8 GPIO_State);
+
+/*****************************************************
+ * Function: GPIO_getPinValue
+ * Description: Reads the current state of a specified GPIO pin.
+ *
+ * Parameters:
+ *   - GPIO_Port: Pointer to the GPIO port to which the pin belongs.
+ *   - GPIO_pin: The GPIO pin number to read.
+ *   - GPIO_State: Pointer to store the read state of the GPIO pin (GPIO_STATE_SET or GPIO_STATE_RESET).
+ *
+ * Return:
+ *   - GPIO_ErrorStatus_t: An error status indicating the success or failure of the operation.
+ *
+ * Errors:
+ *   - GPIO_NULLPTR: Returned if the GPIO_Port pointer is NULL.
+ *   - GPIO_InvalidPin: Returned if an invalid GPIO pin is provided.
+ *   - GPIO_OK: Returned if the operation is successful.
+ *
+ * Usage: GPIO_ErrorStatus_t error; u8 state; error = GPIO_getPinValue(GPIO_PORT_A, GPIO_PIN_5, &state);
+ * Example: Read the state of GPIO pin 5 on port A and store it in the 'state' variable.
+ *
+ * Notes:
+ *   - Ensure that no errors are present before using the GPIO pin state.
+ *****************************************************/
+GPIO_ErrorStatus_t GPIO_getPinValue(void* GPIO_Port, u8 GPIO_pin, u8* GPIO_State);
+
+
+#endif /* GPIO_H_ */
