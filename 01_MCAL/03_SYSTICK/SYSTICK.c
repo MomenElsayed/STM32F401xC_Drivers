6df2c51bea0ecbef8a6e227ba4425b113d45af40
Momen Elsayed <momen98s@gmail.com>
2024-03-11 17:50:03 +0200

    SysTick MCAL V01

---
+++ 01_MCAL/03_SYSTICK/SYSTICK.c
@@ -0,0 +1,103 @@
+/******************************************************************************
+ *
+ * Module: SysTick
+ *
+ * File Name: SYSTICK.c
+ *
+ * Description: Source file for the SysTick driver for STM32F401xC
+ *
+ * Author: Momen Elsayed Shaban
+ *
+ *******************************************************************************/
+#include "SYSTICK.h"
+
+
+#define SYSTICK_BASE_ADDR			0xE000E010
+#define SYSTICK_START_MASK			0xFFFFFFF8
+#define SYSTICK_MAX_LOAD_VAL		0x00FFFFFF
+#define SYSTICK_MAX_CALL_BACK_FN	3
+
+typedef struct{
+	volatile u32 STK_CTRL;
+	volatile u32 STK_LOAD;
+	volatile u32 STK_VAL;
+	volatile u32 STK_CALIB;
+}SYSTICK_Registers_t;
+
+
+static SYSTICK_Registers_t* const SYSTICK = (SYSTICK_Registers_t*)SYSTICK_BASE_ADDR;
+
+
+static SYSTICKCallBackFn_t App_CBF[SYSTICK_MAX_CALL_BACK_FN];
+
+SYSTICK_ErrorStatus_t SYSTICK_start(u32 SYSTICK_Clk)
+{
+	SYSTICK_ErrorStatus_t Error_Status = SYSTICK_OK;
+	u32 Systick_ctrl_reg = 0;
+	if((SYSTICK_Clk != SYSTICK_CLK_AHB) && (SYSTICK_Clk != SYSTICK_CLK_AHB_DIV_8))
+	{
+		Error_Status = SYSTICK_InvalidClkSrcValue;
+	}
+	else
+	{
+		Systick_ctrl_reg = SYSTICK->STK_CTRL;
+		Systick_ctrl_reg &= SYSTICK_START_MASK;
+		Systick_ctrl_reg |= SYSTICK_Clk;
+		SYSTICK->STK_CTRL = Systick_ctrl_reg;
+	}
+	return Error_Status;
+}
+
+SYSTICK_ErrorStatus_t SYSTICK_setTicks(u32 SYSTICK_ticks)
+{
+	SYSTICK_ErrorStatus_t Error_Status = SYSTICK_OK;
+	if (SYSTICK_ticks > SYSTICK_MAX_LOAD_VAL)
+	{
+		Error_Status = SYSTICK_InvalidTicksValue;
+	}
+	else
+	{
+		SYSTICK->STK_LOAD = SYSTICK_ticks;
+	}
+	return Error_Status;
+}
+
+SYSTICK_ErrorStatus_t SYSTICK_Stop()
+{
+	SYSTICK_ErrorStatus_t Error_Status = SYSTICK_OK;
+	SYSTICK->STK_CTRL = 0;
+	return Error_Status;
+}
+
+SYSTICK_ErrorStatus_t SYSTICK_setCallBack(SYSTICKCallBackFn_t SYSTICK_CBF, u8 req_Index)
+{
+	SYSTICK_ErrorStatus_t Error_Status = SYSTICK_OK;
+	if(req_Index > SYSTICK_MAX_CALL_BACK_FN)
+	{
+		Error_Status = SYSTICK_InvalidCallBackIndex;
+	}
+	else if (!SYSTICK_CBF)
+	{
+		Error_Status = SYSTICK_NullPtr;
+	}
+	else
+	{
+		App_CBF[req_Index] = SYSTICK_CBF;
+	}
+	return Error_Status;
+}
+
+void SysTick_Handler(void)
+{
+	u8 iterator = 0;
+	for(iterator = 0 ; iterator < SYSTICK_MAX_CALL_BACK_FN ; iterator++)
+	{
+		if(App_CBF[iterator])
+		{
+			App_CBF[iterator]();
+		}
+	}
+
+}
+
+
---
+++ 01_MCAL/03_SYSTICK/SYSTICK.h
@@ -0,0 +1,130 @@
+/*
+ * SYSTICK.h
+ *
+ *  Created on: 11 Mar 2024
+ *      Author: Momen El Sayed
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include "std_types.h"
+/*******************************************************************************
+ *                                Type Decelerations                           *
+ *******************************************************************************/
+
+#define SYSTICK_CLK_AHB 			0x00000007
+#define SYSTICK_CLK_AHB_DIV_8 		0x00000003
+
+typedef enum{
+	SYSTICK_OK,
+	SYSTICK_InvalidClkSrcValue,
+	SYSTICK_InvalidTicksValue,
+	SYSTICK_InvalidCallBackIndex,
+	SYSTICK_NullPtr
+}SYSTICK_ErrorStatus_t;
+
+typedef void (*SYSTICKCallBackFn_t) (void);
+
+
+/*******************************************************************************
+ *                              Functions Prototypes                           *
+ *******************************************************************************/
+/*****************************************************
+ * Function: SYSTICK_start
+ * Description: Configures and starts the SysTick timer with the specified clock source.
+ *
+ * Parameters:
+ *   - SYSTICK_Clk: Clock source for SysTick timer.
+ *                  - SYSTICK_CLK_AHB: Use AHB clock as the source.
+ *                  - SYSTICK_CLK_AHB_DIV_8: Use AHB/8 clock as the source.
+ *
+ * Return:
+ *   - SYSTICK_ErrorStatus_t: Status of the operation.
+ *     - SYSTICK_OK: Operation successful.
+ *     - SYSTICK_InvalidClkSrcValue: Returned if an invalid clock source value is provided.
+ *
+ * Usage:
+ *   SYSTICK_ErrorStatus_t status = SYSTICK_start(SYSTICK_CLK_AHB);
+ *   if(status == SYSTICK_OK) {
+ *     // SysTick timer started successfully
+ *   }
+ *
+ * Notes:
+ *   - This function configures and initiates the SysTick timer with the specified clock source.
+ *   - The SysTick timer is often used for generating time delays.
+ *****************************************************/
+SYSTICK_ErrorStatus_t SYSTICK_start(u32 SYSTICK_Clk);
+
+/*****************************************************
+ * Function: SYSTICK_setTicks
+ * Description: Sets the number of ticks for the SysTick timer.
+ *
+ * Parameters:
+ *   - SYSTICK_ticks: Number of ticks to be set for the SysTick timer.
+ *
+ * Return:
+ *   - SYSTICK_ErrorStatus_t: Status of the operation.
+ *     - SYSTICK_OK: Operation successful.
+ *     - SYSTICK_InvalidTicksValue: Returned if an invalid number of ticks is provided.
+ *
+ * Usage:
+ *   SYSTICK_ErrorStatus_t status = SYSTICK_setTicks(1000);
+ *   if(status == SYSTICK_OK) {
+ *     // SysTick ticks set successfully
+ *   }
+ *
+ * Notes:
+ *   - This function sets the number of ticks for the SysTick timer, determining the period of interrupts.
+ *   - The provided number of ticks should not exceed the maximum load value (SYSTICK_MAX_LOAD_VAL).
+ *****************************************************/
+SYSTICK_ErrorStatus_t SYSTICK_setTicks(u32 SYSTICK_ticks);
+
+/*****************************************************
+ * Function: SYSTICK_Stop
+ * Description: Stops the SysTick timer.
+ *
+ * Parameters: None
+ *
+ * Return:
+ *   - SYSTICK_ErrorStatus_t: Status of the operation.
+ *     - SYSTICK_OK: Operation successful.
+ *
+ * Usage:
+ *   SYSTICK_ErrorStatus_t status = SYSTICK_Stop();
+ *   if(status == SYSTICK_OK) {
+ *     // SysTick timer stopped successfully
+ *   }
+ *
+ *****************************************************/
+SYSTICK_ErrorStatus_t SYSTICK_Stop();
+
+/*****************************************************
+ * Function: SYSTICK_setCallBack
+ * Description: Sets the callback function for a specific SysTick timer event.
+ *
+ * Parameters:
+ *   - SYSTICK_CBF: Pointer to the callback function to be set.
+ *   - req_Index: Index representing the SysTick timer event for which the callback is set.
+ *
+ * Return:
+ *   - SYSTICK_ErrorStatus_t: Status of the operation.
+ *     - SYSTICK_OK: Operation successful.
+ *     - SYSTICK_InvalidCallBackIndex: Returned if an invalid callback index is provided.
+ *     - SYSTICK_NullPtr: Returned if the provided callback function pointer is NULL.
+ *
+ * Usage:
+ *   SYSTICK_ErrorStatus_t status = SYSTICK_setCallBack(MyCallbackFunction, 0);
+ *   if(status == SYSTICK_OK) {
+ *     // Callback function set successfully for SysTick event at index 0
+ *   }
+ *
+ * Notes:
+ *   - This function associates a callback function with a specific SysTick timer event, allowing custom actions
+ *     to be taken when the corresponding event occurs.
+ *   - The callback function pointer must be valid (not NULL), and the index should be within the valid range.
+ *****************************************************/
+SYSTICK_ErrorStatus_t SYSTICK_setCallBack(SYSTICKCallBackFn_t SYSTICK_CBF, u8 req_Index);
+
+
+#endif /* SYSTICK_H_ */
